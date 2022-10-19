5a0e79f5396b329511085aca7290d5e85a6fb692
Taehoon Yoon <thyoon515@gmail.com>
2022-10-19 14:42:36 -0400

    commit tree b8cc79b5937208e3f60e91d7475ac2023fcdbc8b

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: {in: ['Fiction', 'Non-Fiction']}
+    validates :title, exclusion: {in: ['True Facts']}
 end
